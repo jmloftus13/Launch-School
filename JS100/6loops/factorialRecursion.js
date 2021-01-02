c2c968b0059cbc64a99a09e8d3fa7b706d2cac34
Jeannine Loftus <jmloftus13@yahoo.com>
2021-01-02 14:36:55 -0500

    updating loop files

---
+++ JS100/6loops/factorialRecursion.js
@@ -0,0 +1,16 @@
+console.clear();
+
+function factorial(num) { 
+  if (num === 1) { 
+    return 1;
+  }
+  return num * factorial(num - 1);
+};
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
+console.log(factorial(6));
+console.log(factorial(7));
+console.log(factorial(8));
